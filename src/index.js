8c43015f7ee90e50a259308fdf990022d054fae8
colo13 <joa_028@hotmail.com>
2021-05-07 20:20:18 -0300

    update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,35 @@ const app = express();
 const portser = 80;
 const path = require('path');
 
-//Server
+
+
+//set 
+app.set('views', path.join(__dirname + '/../assets', 'views'))
 app.set('view engine', 'ejs');
-
-
+app.engine('html', require('ejs').renderFile);
+app.use('/assets', express.static('assets'));
 app.listen(portser, () => {
     console.log('Server on', portser);
 })
 
 app.get('/', (req, res) =>{
-res.render('index', path.join(__dirname + '/../index.html'));
+res.render('index.html');
+})
+app.get('/inicio', (req, res) =>{
+res.render('index.html');
+})
+app.get('/index', (req, res) =>{
+    res.render('indexeng.html');
+    })
+
+app.get('/nosotros', (req, res) =>{
+    res.render('nosotros.html');
+    })
+
+app.get('/contactanos', (req, res) =>{
+    res.render('contactanos.html');
+    })
+
+app.get('/indexeng', (req, res) =>{
+res.render('indexeng.html');
 })
